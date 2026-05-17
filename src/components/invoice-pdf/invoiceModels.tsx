8edc6193f8bfe3ce6ac21317edfefe272e4eac52
beto-freitas <gilbertofreitas997@gmail.com>
2026-05-17 12:24:56 -0300

    fix: re-do table migrations to fix weird issue on smpt & removed footer on invoice

--- src/components/invoice-pdf/invoiceModels.tsx
+++ src/components/invoice-pdf/invoiceModels.tsx
@@ -257,11 +257,6 @@ const InvoicePDFModelBaseV0 = ({ data }: { data: InvoicePDFData }) => {
 					<Text style={styles.totalLabel}>Total</Text>
 					<Text style={styles.totalAmount}>{totalValue}</Text>
 				</View>
-
-				<View style={styles.generatedDivider} />
-				<Text style={styles.generatedWith}>
-					Generated with {appConfig.appName}
-				</Text>
 			</Page>
 		</Document>
 	);
