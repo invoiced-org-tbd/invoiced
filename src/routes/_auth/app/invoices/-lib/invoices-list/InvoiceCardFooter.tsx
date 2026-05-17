06cbdf27a5d8875caf1ede29541573ea29c1e617
Beto Freitas <gilbertofreitas997@gmail.com>
2026-05-17 10:22:15 -0300

    feat: invoice download and proper lastInvoiceNumber update (#54)

--- src/routes/_auth/app/invoices/-lib/invoices-list/InvoiceCardFooter.tsx
+++ src/routes/_auth/app/invoices/-lib/invoices-list/InvoiceCardFooter.tsx
@@ -1,19 +1,37 @@
-import { Card } from '@/components/card/Card';
-import { TrashIcon } from 'lucide-react';
+import type { GetInvoicesResponse } from '@/api/invoice/getInvoices';
 import { Button } from '@/components/button/Button';
+import { Card } from '@/components/card/Card';
 import { useTranslate } from '@/hooks/use-translate/useTranslate';
 import { getRouteApi } from '@tanstack/react-router';
+import { DownloadIcon, TrashIcon } from 'lucide-react';
+import { useDownloadInvoice } from '../useDownloadInvoice';
 
 const invoicesRouteApi = getRouteApi('/_auth/app/invoices/');
 
-export const InvoiceCardFooter = () => {
+type InvoiceCardFooterProps = {
+	invoice: GetInvoicesResponse[number];
+};
+
+export const InvoiceCardFooter = ({ invoice }: InvoiceCardFooterProps) => {
 	const { t } = useTranslate();
+	const { downloadInvoice, isDownloading } = useDownloadInvoice();
 
 	return (
 		<Card.Footer className='flex-row justify-between items-center border-t pt-5 mx-2 px-2'>
 			<div></div>
 
-			<div>
+			<div className='flex items-center gap-2'>
+				<Button
+					variant='secondary'
+					size='xxs'
+					isOutlined
+					isLoading={isDownloading}
+					onClick={() => downloadInvoice(invoice)}
+				>
+					<DownloadIcon />
+					{t('invoices.list.downloadInvoice')}
+				</Button>
+
 				<invoicesRouteApi.Link
 					to='.'
 					search={(prev) => ({ ...prev, isDeletingInvoice: true })}
